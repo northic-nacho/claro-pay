e28790ebc7a0c85ddfa369e2a1f087db3ef9473c
Max Caluva <max.caluva@gmail.com>
2020-08-02 00:06:14 -0300

    Add SectionDescription

---
+++ src/Components/SectionDescription/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SectionDescription = ({ children }) => {
+  return (
+    <div className="section-description">
+      <p className="description">{children}</p>
+    </div>
+  );
+};
+
+export default SectionDescription;
