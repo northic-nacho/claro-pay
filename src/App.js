7857dbf37dea037cbc3cc3e17d7dc1a09107a227
Max Caluva <max.caluva@gmail.com>
2020-08-02 00:06:26 -0300

    Formatting

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Footer from "./Components/Footer";
 import Paginator from "./Components/Paginator";
 import NavBar from "./Components/NavBar";
 
-import './styles/index.scss';
+import "./styles/index.scss";
 
 function App() {
   React.useEffect(() => {
