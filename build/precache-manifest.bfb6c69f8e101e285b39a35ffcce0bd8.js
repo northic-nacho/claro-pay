ba782e7553ec1855768ab11a891fae87792af74f
northic-nacho <nacho@northic.io>
2020-09-23 18:29:25 -0300

    change branch to deploy

---
+++ build/precache-manifest.bfb6c69f8e101e285b39a35ffcce0bd8.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ceff80db67f704ca06f77d6085eee80",
+    "url": "/claro-pay/index.html"
+  },
+  {
+    "revision": "a9fa3d5a6ea3dbcdcb6e",
+    "url": "/claro-pay/static/css/main.99f0d72d.chunk.css"
+  },
+  {
+    "revision": "5e8a1fe0891a9041da68",
+    "url": "/claro-pay/static/js/2.4dd9571d.chunk.js"
+  },
+  {
+    "revision": "e99e5765bb53969b57bc54d653abf826",
+    "url": "/claro-pay/static/js/2.4dd9571d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9fa3d5a6ea3dbcdcb6e",
+    "url": "/claro-pay/static/js/main.570f7a8a.chunk.js"
+  },
+  {
+    "revision": "09c83dc45f46c069b6f9",
+    "url": "/claro-pay/static/js/runtime-main.bff85c03.js"
+  },
+  {
+    "revision": "003f20b8fadfc653a0a2c2ed00472053",
+    "url": "/claro-pay/static/media/Gotham-Bold.003f20b8.svg"
+  },
+  {
+    "revision": "3c99dc65cf2e00e337443bc1d137ef22",
+    "url": "/claro-pay/static/media/Gotham-Bold.3c99dc65.ttf"
+  },
+  {
+    "revision": "8c607d93d55a883a7e6e5984bdece7dd",
+    "url": "/claro-pay/static/media/Gotham-Bold.8c607d93.eot"
+  },
+  {
+    "revision": "9efcb38f733b45b88df3a3661e37889a",
+    "url": "/claro-pay/static/media/Gotham-Bold.9efcb38f.woff"
+  },
+  {
+    "revision": "68a642cc3058e5bc7aaa9301915b68c1",
+    "url": "/claro-pay/static/media/Gotham-Light.68a642cc.woff"
+  },
+  {
+    "revision": "823148ce33e83fc113a926e01030699a",
+    "url": "/claro-pay/static/media/Gotham-Light.823148ce.svg"
+  },
+  {
+    "revision": "beaaefbf0dc98a4ef78a1eb17925bc57",
+    "url": "/claro-pay/static/media/Gotham-Light.beaaefbf.ttf"
+  },
+  {
+    "revision": "e99d9ff21bf6d27614272d5d70cd0462",
+    "url": "/claro-pay/static/media/Gotham-Light.e99d9ff2.eot"
+  },
+  {
+    "revision": "0b28710a605aed91211bd25a19081bae",
+    "url": "/claro-pay/static/media/Gotham-Medium.0b28710a.ttf"
+  },
+  {
+    "revision": "36b072fbce090fc73bdc2716ae875561",
+    "url": "/claro-pay/static/media/Gotham-Medium.36b072fb.woff"
+  },
+  {
+    "revision": "dd55072870b528ad7c10985dc4f0294a",
+    "url": "/claro-pay/static/media/Gotham-Medium.dd550728.svg"
+  },
+  {
+    "revision": "e09950af496772a48efb6c628a1f6844",
+    "url": "/claro-pay/static/media/Gotham-Medium.e09950af.eot"
+  },
+  {
+    "revision": "4a20b1fbaa3071effb96869b97dbc9de",
+    "url": "/claro-pay/static/media/bg-s1.4a20b1fb.jpg"
+  },
+  {
+    "revision": "056fa84e3d6f8908acd125cba210fdca",
+    "url": "/claro-pay/static/media/copyfonts.com_gotham-black.056fa84e.ttf"
+  },
+  {
+    "revision": "1e6da042bad7039303cea7cec0fba4d0",
+    "url": "/claro-pay/static/media/copyfonts.com_gotham-black.1e6da042.woff"
+  },
+  {
+    "revision": "b57fd6f541cae971b425ba06abd1596d",
+    "url": "/claro-pay/static/media/copyfonts.com_gotham-black.b57fd6f5.svg"
+  },
+  {
+    "revision": "ccb3c87d39770c1c0d5c1fbdd69650b0",
+    "url": "/claro-pay/static/media/copyfonts.com_gotham-black.ccb3c87d.eot"
+  },
+  {
+    "revision": "201824ba4089c35ea997b3018e2bfbba",
+    "url": "/claro-pay/static/media/copyfonts.com_gotham-book.201824ba.eot"
+  },
+  {
+    "revision": "42abce9b6ccc89ccd364ae9989150ad3",
+    "url": "/claro-pay/static/media/copyfonts.com_gotham-book.42abce9b.svg"
+  },
+  {
+    "revision": "c903d59489eb7600df5665f20f363bf7",
+    "url": "/claro-pay/static/media/copyfonts.com_gotham-book.c903d594.ttf"
+  },
+  {
+    "revision": "e1d0c1f01111646c51bb8d735484ef9b",
+    "url": "/claro-pay/static/media/copyfonts.com_gotham-book.e1d0c1f0.woff"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/claro/precache-manifest.42129900b06a7a0e294e2b13a326521e.js"
+  "/claro-pay/precache-manifest.bfb6c69f8e101e285b39a35ffcce0bd8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/claro/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/claro-pay/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
